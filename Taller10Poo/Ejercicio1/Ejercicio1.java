938fbdc6c882908bc1abd409a11412398be57504
BreinerMontes01 <breiner.montespetro@unicolombo.edu.co>
2024-11-23 21:11:03 -0500

    Taller 10

---
+++ Taller10Poo/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,13 @@
+package Taller10Poo.Ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        Persona estudiante = new Estudiante();
+        Persona profesor = new Profesor();
+        
+        persona.presentarse();
+        estudiante.presentarse();
+        profesor.presentarse();
+    }
+}
---
+++ Taller10Poo/Ejercicio2/Ejercicio2.java
@@ -0,0 +1,11 @@
+package Taller10Poo.Ejercicio2;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Vehiculo vehiculo = new Vehiculo();
+        Vehiculo bicicleta = new Bicicleta();
+        
+        vehiculo.moverse();
+        bicicleta.moverse();
+    }
+}
